1eef9521633ba9963565b1a42e52aa193a54bbca
sanniu <niuzhikui@qq.com>
2014-11-12 12:20:51 +0800

    first

---
+++ AndroidSkinService/src/main/gen/com/simon/catkins/skin/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.simon.catkins.skin;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ AndroidSkinServiceSample/src/main/java/com/simon/catkins/skin/sample/MainActivity.java
@@ -0,0 +1,48 @@
+package com.simon.catkins.skin.sample;
+
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.Menu;
+
+import com.simon.catkins.skin.SkinService;
+import com.simon.catkins.skin.external.ExternalSkin;
+
+public class MainActivity extends Activity {
+
+    private ExternalSkin mExternalSkin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getLayoutInflater().setFactory(SkinService.getInflatorFactory(this));
+        super.onCreate(savedInstanceState);
+
+        mExternalSkin = new ExternalSkin("/sdcard/main.zip",
+                getResources().getDisplayMetrics(),
+                getResources().getConfiguration());
+
+        SkinService.addSkin(mExternalSkin);
+
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SkinService.applySkin(this, ExternalSkin.NAME);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mExternalSkin.updateConfiguration(getResources().getDisplayMetrics(), newConfig);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
